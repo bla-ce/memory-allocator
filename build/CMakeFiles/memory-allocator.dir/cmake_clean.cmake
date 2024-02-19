9602601b700df68bff46b5953e33c121b0166e85
bla-ce <contact.charaud@gmail.com>
2024-02-20 09:45:08 +1000

    setup cmake

---
+++ build/CMakeFiles/memory-allocator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/memory-allocator.dir/src/main.cpp.o"
+  "CMakeFiles/memory-allocator.dir/src/main.cpp.o.d"
+  "memory-allocator"
+  "memory-allocator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/memory-allocator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
